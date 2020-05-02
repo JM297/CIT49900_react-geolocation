842b5c39a05eea78beba6a31e8da31c5e9e74057
JM297 <46730549+JM297@users.noreply.github.com>
2020-05-02 01:46:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ export default function App() {
     const [error, setError] = React.useState(null);
     const [color, setColor] = React.useState("red");
     const [destination, setDestination] = React.useState({
-        x:39.768996942,
-        y:-86.17166598
+        x1:39.734412,
+        y1:-86.198831,
+        x2:39.734319,
+        y2:-86.198729
     });
     const hitbox = {
         height:100,
@@ -35,9 +37,11 @@ export default function App() {
             setError("Hey, this isn't working!");
             return;
         }
-        if(position.x === destination.x && position.y === destination.y){
-            setColor("green");
-            setGoal("You have arrived");
+        if(position.x >= destination.x1 && position.x <= destination.x2){
+            if(position.y >= destination.y1 && position.y <= destination.y2){
+                setColor("green");
+                setGoal("You have arrived");
+            }
         } else{
             setColor("red");
             setGoal("Not close");
@@ -45,8 +49,11 @@ export default function App() {
         let watcher = geo.watchPosition(onChange,onError);
         return () => geo.clearWatch(watcher);
     },[destination,position]);
-    console.log(destination.x);
-    console.log(position.x);
+    console.log(destination.x1);
+    console.log(destination.x2);
+    console.log(destination.y1);
+    console.log(destination.y2);
+    console.log(position.x, position.y);
 
   return (
     <div className="App">
